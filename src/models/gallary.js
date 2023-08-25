771f68a84b8ab4114f19cb2a5c0953f388dc29d4
msjadhav03 <manishajadhav2323@gmail.com>
2023-08-25 19:56:10 +0530

    added changes for server

--- src/models/gallary.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const imageSchema = new mongoose.Schema({
-    data: mongoose.Schema.Types.Buffer,
-    contentType: String
-});
-
-module.exports = mongoose.model('Image', imageSchema);
